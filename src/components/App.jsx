98fecb51c105841c7cd045711365d2e288fe447c
naizirhabib <naizir68@gmail.com>
2025-07-17 12:09:47 +0930

    Sprint 14, dynamic routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,23 @@ import Hobbies from "./AboutMe/Hobbies"
 import MyStory from "./AboutMe/MyStory";
 import SiteHistory from "./AboutUs/SiteHistory";
 import SiteMission from "./AboutUs/SiteMission"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    // Obtiene los datos de reseñas del servidor.
+    fetch('https://emoji-critic.es.tripleten-services.com/v1/reviews').then((res) => {
+      return res.json();
+    }).then((data) => {
+      // Pasa el cuerpo de la respuesta analizada a la función setter.
+      console.log(data);
+      setReviews(data);
+    })
+    .catch(console.error);
+  // Un array de dependencia vacío significa que el hook sólo se ejecuta cuando
+  // se carga el componente.
+  }, []);
   return (
     <div className="App">
       <Header />
