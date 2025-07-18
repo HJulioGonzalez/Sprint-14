3d475cf605a295eeb9ed69c7a0a8849d3a45f8be
Habib Julio <89555113+HJulioGonzalez@users.noreply.github.com>
2025-07-18 10:06:11 +0930

    Sprint 14 - dynamic routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,49 @@
 import "./App.css";
 import Header from "./Header/Header";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./Dashboard/Dashboard"
-import Reviews from "./Reviews/Reviews"
-import AboutMe from "./AboutMe/AboutMe"
-import AboutUs from "./AboutUs/AboutUs"
-import Contact from "./AboutMe/Contact"
-import Hobbies from "./AboutMe/Hobbies"
+import Dashboard from "./Dashboard/Dashboard";
+import Reviews from "./Reviews/Reviews";
+import Review from "./Review/Review";
+import AboutMe from "./AboutMe/AboutMe";
+import AboutUs from "./AboutUs/AboutUs";
+import Contact from "./AboutMe/Contact";
+import Hobbies from "./AboutMe/Hobbies";
 import MyStory from "./AboutMe/MyStory";
 import SiteHistory from "./AboutUs/SiteHistory";
-import SiteMission from "./AboutUs/SiteMission"
+import SiteMission from "./AboutUs/SiteMission";
 import { useState, useEffect } from "react";
 
 function App() {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    // Obtiene los datos de reseñas del servidor.
-    fetch('https://emoji-critic.es.tripleten-services.com/v1/reviews').then((res) => {
-      return res.json();
-    }).then((data) => {
-      // Pasa el cuerpo de la respuesta analizada a la función setter.
-      console.log(data);
-      setReviews(data);
-    })
-    .catch(console.error);
-  // Un array de dependencia vacío significa que el hook sólo se ejecuta cuando
-  // se carga el componente.
+    fetch("https://emoji-critic.es.tripleten-services.com/v1/reviews")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setReviews(data);
+      })
+      .catch(console.error);
   }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Dashboard/>}/>
-        <Route path="/reviews" element={<Reviews/>}/>
-        <Route path="/about-me" element={<AboutMe/>}>
-          <Route path="contact" element={<Contact/>}/>
-          <Route path="hobbies" element={<Hobbies/>}/>
-          <Route path="my-story" element={<MyStory/>}/>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/:reviewId"
+          element={<Review reviews={reviews} />}
+        />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
         </Route>
-        <Route path="/about-us" element={<AboutUs/>}>
-          <Route path="site-history" element={<SiteHistory/>}/>
-          <Route path="site-mission" element={<SiteMission/>}/>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="site-history" element={<SiteHistory />} />
+          <Route path="site-mission" element={<SiteMission />} />
         </Route>
       </Routes>
     </div>
